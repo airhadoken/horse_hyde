981996714423e97df665bd2a5432bcd29fd697b0
Bradley Momberger <air_hadoken@yahoo.com>
2014-03-19 23:14:22 -0700

    Better handling of two word phrases, more probability/branching added

--- horse_hyde.js
+++ horse_hyde.js
@@ -1,7 +1,8 @@
-var terminals = {};
-var startwords = [];
+var startwords;
 var wordstats = {};
-var wordstats22 = {};
+var words, lastword; // these are used in the for loop below
+var LINEBREAK = "/\n";
+var NOTERMREGEX = /\b(an?|the|i)$/;
 
 var fs = require('fs');
 
@@ -25,6 +26,7 @@ try {
 catch (err) {
   console.error("There was an error opening the file " + file + ":");
   console.log(err);
+  process.exit(-1);
 }
 });
 var titles = lines;
@@ -34,30 +36,36 @@ for (var i = 0; i < titles.length; i++) {
     var words = titles[i];
     if(!words)
       continue;
+    // strip all punctuation from words except for apostrophes
     words = words.split(' ')
             .map(function(word) { return word.replace(/[^a-zA-Z']/g, "").toLowerCase().trim(); })
             .filter(function(word) { return word.trim().length; });
     if(words.length < 1)
       continue;
-    terminals[words[words.length-1]] = true;
-    ~startwords.indexOf(words[0]) || startwords.push(words[0]);
     for (var j = 0; j < words.length - 1; j++) {
-        if (wordstats.hasOwnProperty(words[j])) {
-            wordstats[words[j]].push(words[j+1]);
+      // This isn't perfect, since we stop at line breaks
+      function pushnew(key, next) {
+        if (wordstats.hasOwnProperty(key)) {
+            wordstats[key].push(next);
         } else {
-            wordstats[words[j]] = [words[j+1]];
+            wordstats[key] = [next];
         }
+      }
+      pushnew(words[j], words[j+1]);
+      if(words[j+2]) {
+        pushnew(words[j], words[j+1] + " " + words[j+2]);
+      }
 
-        if(j < words.length - 3) {
-          var _22word = words[j] + ' ' + words[j + 1];
-          if (wordstats22.hasOwnProperty(_22word)) {
-              wordstats22[_22word].push(words[j+2] + ' ' + words[j+3]);
-          } else {
-              wordstats22[_22word] = [words[j+2] + ' ' + words[j+3]];
-          }
+      if(lastword) {
+        pushnew(lastword + " " + words[j], words[j+1]);
+        if(words[j+2]) {
+          pushnew(lastword + " " + words[j], words[j+1] + " " + words[j+2]);
         }
+      }
+      lastword = words[j];
     }
 }
+startwords = Object.keys(wordstats);
 
 var choice = function (a) {
     var i = Math.floor(a.length * Math.random());
@@ -65,22 +73,58 @@ var choice = function (a) {
 };
 
 var make_title = function (min_length) {
-    var word = choice(startwords);
-    var title = [word];
-    while (title.join(' ').length < min_length) {
-        var next_words;
-        if(title.length > 1 && Math.random() > 0.3) {
-          next_words = wordstats22[title[title.length - 2] + ' ' + word] || wordstats[word] || startwords;
-        } else {
-          next_words = wordstats[word] || startwords;
-        }
-        word = choice(next_words);
-        title.push(word);
-        //if (title.length > min_length && terminals.hasOwnProperty(word)) break;
+    // Underwold thing: put a repeating word at the end of every phrase
+    // i.e. "hands girls boy / and steel boy / you had chemicals boy / I've grown so close to you boy"
+    // Chance 25%.
+    //  If two word phrase chosen from corpus, 50% chance of using just the second word 
+    var stopword = Math.random() < 0.25 ? choice(startwords) : null;
+    if(stopword && ~stopword.indexOf(" ") && Math.random() < 0.5) {
+      stopword = stopword.substr(stopword.indexOf(" ") + 1);
     }
-    if (title.join(' ').length < min_length) return make_title(min_length);
+    var word = [choice(startwords)];
+    var title = word.slice(0);
+    var next_words;
+    while (title.join(' ').length < min_length || NOTERMREGEX.test(word.join(" "))) {
+        next_words = null;
+        // 30% chance of trying to find a key in the corpus from the last two words
+        //  combined. (frequency weighted)
+        // Avoid combining with a previous newline. Pick the word before the newline
+        //  if you encounter it.
+        if(title.length > 1 && Math.random() < 0.3) {
+          next_words = wordstats[word.length > 1 
+                                 ? word.join(" ") 
+                                 : (title[title.length - 2]===LINEBREAK 
+                                    ? title[title.length-3] 
+                                    : title[title.length-2])
+                                   + " " + word[0]
+                                 ];
+        }
+        // 56-80% chance (depends on whether key is found above) of finding next
+        // Markov step based on the last word alone. (frequency weighted)
+        if(Math.random() < 0.8) {
+          next_words = next_words || wordstats[word[1] || word[0]];        
+        }
+        // 14-20% chance of random word from corpus (evenly weighted)
+        next_words = next_words || startwords;
+        word = choice(next_words);
+        word = word.split(" ");
+        title = title.concat(word);
+        // After 1-2 words have been added, 30% chance of splitting
+        //  to new line.  Use stopword if it has been selected.
+        if(!NOTERMREGEX.test(word.join(" ")) && Math.random() < 0.3) {
+          stopword && title.push(stopword);
+          title.push(LINEBREAK);
+        }
+    }
+    // Don't end with a linebreak (slash-then-newline).
+    if(title[title.length - 1] === LINEBREAK) {
+      title.pop();
+    }
     return title.join(' ');
 };
 
-console.log(make_title(100));
-process.exit();
+var m = make_title(100);
+console.log(m);
+setTimeout(function() {
+  process.exit(0);
+}, 100);
